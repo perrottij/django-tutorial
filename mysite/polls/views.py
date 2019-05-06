2223eda1378f9832fd7fe67d7470d2cc8f19b686
Perrottij <Perrottij@knights.local>
2019-05-06 09:04:03 -0400

    beginning to track the database

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -8,7 +8,7 @@ from django.urls import reverse
 from .models import Question, Choice
 
 def index(request):
-        latest_question_list = Question.objects.order_by('-pub_date')[:5]
+        latest_question_list = Question.objects.order_by('-pub_date')[:10]
         context = {'latest_question_list': latest_question_list}
         return render(request, 'polls/index.html', context)
 
