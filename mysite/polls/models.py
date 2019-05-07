29486809b1e27ed0a99ba70440c300b57144238a
Perrottij <Perrottij@knights.local>
2019-05-07 09:05:17 -0400

    adding images

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -5,6 +5,7 @@ from django.db import models
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
+    img = models.URLField(max_length=200, blank=True)
 
     def __str__(self):
         return self.question_text
